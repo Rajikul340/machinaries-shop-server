9360325dbc39d90ec68fcf7f5573541f322e8124
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-11-24 20:21:27 +0600

    getProductBy_category

--- index.js
+++ index.js
@@ -16,15 +16,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const MachineCollection = client.db('machineCollection').collection('latheMachine');
+const MachineCollection = client.db('machineCollection').collection('AllMachine');
+const categoryCollection = client.db('machineCollection').collection('machine_category');
 
 
 async function run(){
     try{
-
-        app.get('/lathMachine', async(req, res)=>{
+         //get all machine by category 
+        app.get('/AllMachine/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query ={category_id:id}
+            const cursor = await MachineCollection.findOne(query)
+            res.send(cursor)
+            console.log(cursor);
+        })
+        //get all machine category
+        app.get('/machine_category', async(req, res)=>{
             const query ={}
-            const cursor = await MachineCollection.find(query).toArray();
+            const cursor = await categoryCollection.find(query).toArray();
             res.send(cursor)
             console.log(cursor);
         })
